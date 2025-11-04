6f9d42aa73830c905f54779202808d861213d4a3
Olivier <opronieradagio.io>
2025-11-04 19:09:11 +0100

    Correcting: Block runchecks if not complaint

--- src/main.js
+++ src/main.js
@@ -10,11 +10,17 @@ export async function main() {
     buildApp();
 
     // Try to reach the Adagio API
+    if (typeof ADAGIO_KEY === 'undefined') window.ADAGIO_KEY = '';
     const apiReadyCall = await runAdagioApiQuery('https://api.adagio.io/api/v1/profile');
 
     // A wrapper must be detected and the API must be reachable to run the checks
-    if (prebidObject === undefined) appendHomeContainer(`Prebid.js: 🔴 No wrapper detected - Try to reload checker / page.`);
-    else if (apiReadyCall === null) appendHomeContainer(`Adagio API: 🔴 Not reachable - Contact SE.`);
+    if (prebidObject === undefined) {
+        appendHomeContainer(`Prebid.js: 🔴 No wrapper detected - Try to reload checker / page.`);
+        return;
+    } else if (apiReadyCall === null) {
+        appendHomeContainer(`Adagio API: 🔴 Not reachable - Contact SE.`);
+        return;
+    }
 
     // Tests past all conditions, run the checks
     await runChecks();
